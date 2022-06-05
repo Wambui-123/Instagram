6d2598fd6ddf96bb6fccadccd5f9dcc7b39a1adc
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-06-05 11:52:40 +0300

    added likes functionality

--- instagram_clone/post/urls.py
+++ instagram_clone/post/urls.py
@@ -1,8 +1,9 @@
-from post.views import index, NewPost, PostDetails
+from post.views import index, NewPost, PostDetails, like
 from django.urls import path
 
 urlpatterns = [
     path('', index, name='index'),
     path('newpost/', NewPost, name='newpost'),
     path('<uuid:post_id>', PostDetails, name='postdetails'),
+    path('<uuid:post_id>/like', like, name='postlike'),
 ]

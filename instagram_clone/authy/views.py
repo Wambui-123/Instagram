313c4608eb75db41ceee667891ed58c92a85920b
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-06-05 15:26:03 +0300

    creating follow function

--- instagram_clone/authy/views.py
+++ instagram_clone/authy/views.py
@@ -6,8 +6,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import update_session_auth_hash
 
 from authy.models import Profile
+from post.models import Post, Follow, Stream
+from django.db import transaction
 from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 from django.core.paginator import Paginator
 
@@ -99,3 +103,25 @@ def EditProfile(request):
 	}
 
 	return render(request, 'edit_profile.html', context)
+
+@login_required
+def follow(request, username, option):
+	following = get_object_or_404(User, username=username)
+
+	try:
+		f, created = Follow.objects.get_or_create(follower=request.user, following=following)
+
+		if int(option) == 0:
+			f.delete()
+			Stream.objects.filter(following=following, user=request.user).all().delete()
+		else:
+			 posts = Post.objects.all().filter(user=following)[:25]
+
+			 with transaction.atomic():
+			 	for post in posts:
+			 		stream = Stream(post=post, user=request.user, date=post.posted, following=following)
+			 		stream.save()
+
+		return HttpResponseRedirect(reverse('profile', args=[username]))
+	except User.DoesNotExist:
+		return HttpResponseRedirect(reverse('profile', args=[username]))
--- instagram_clone/instagram_clone/urls.py
+++ instagram_clone/instagram_clone/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from authy.views import UserProfile
+from authy.views import UserProfile, follow
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('authy.urls')),
     path('post/', include('post.urls')),
     path('<username>/', UserProfile, name='profile'),
+    path('<username>/follow/<option>', follow, name='follow'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
